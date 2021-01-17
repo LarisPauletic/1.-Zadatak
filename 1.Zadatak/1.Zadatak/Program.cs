16d3710ce7088fd70c4ad5bd904bb0d82a52c699
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-01-17 13:46:42 +0100

    Add files via upload

---
+++ 1.Zadatak/1.Zadatak/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Hrvatska ima more";
+            string s2 = "Italija je prvak svijeta";
+            string s3 = "Hrvatska je prvak svijeta";
+        }
+    }
+}
